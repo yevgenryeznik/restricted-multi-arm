de24e17e99eb77692144a9e218f790bac8e246cd
Yevgen Ryeznik <yevgen.ryeznik@gmail.com>
2022-05-22 10:54:14 +0200

    MWUD procedure has been added.

---
+++ src/rnd-mwud.jl
@@ -0,0 +1,12 @@
+function mwud(N::Array{Int64}, w::Array{Float64}; α::Float64)
+    # current subject's ID
+    j = sum(N) + 1
+
+    # target allocation proportions
+    ρ = w ./ sum(w)
+    
+    num = (α .* ρ .- N .- (j-1) .* ρ) .* ((α .* ρ .- N .- (j-1) .* ρ) .> 0)
+
+    # probabilities of tretament assignments
+    return num ./ sum(num)
+end
