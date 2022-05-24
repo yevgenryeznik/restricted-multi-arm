08fdb0212ecf8336f671c4cf615f937e61d139ff
Yevgen Ryeznik <yevgen.ryeznik@gmail.com>
2022-05-24 22:50:15 +0200

    Minor changes in simulation file

--- simulation.jl
+++ simulation.jl
@@ -1,10 +1,14 @@
 using CSV
 using Distributions
+using LinearAlgebra
+using StatsBase
 using Random
 
+include("src/simulated-rct.jl")
 include("src/assign-trt.jl")
 include("src/rnd-bud.jl")
 include("src/rnd-pbd.jl")
+include("src/rnd-minqd.jl")
 include("src/randomization.jl")
 
 
@@ -13,4 +17,4 @@ nsim = 10000
 w = float.([4, 3, 2, 1])
 
 
-@time trial = randomize(nsbj, nsim, w, 314159, bud, λ = 1);
+@time trial = randomize(nsbj, nsim, w, 314159, minqd, η = 1.);
