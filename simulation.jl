38c10140fb731c0e21dfcd60f4026535b09d52a2
Yevgen Ryeznik <yevgen.ryeznik@gmail.com>
2022-05-27 09:40:26 +0200

    minor changes in simulations.jl script

--- simulation.jl
+++ simulation.jl
@@ -3,12 +3,14 @@ using Distributions
 using LinearAlgebra
 using StatsBase
 using Random
+using Roots
 
 include("src/simulated-rct.jl")
 include("src/assign-trt.jl")
 include("src/rnd-bud.jl")
 include("src/rnd-pbd.jl")
 include("src/rnd-minqd.jl")
+include("src/rnd-maxent.jl")
 include("src/randomization.jl")
 
 
@@ -18,3 +20,5 @@ w = float.([4, 3, 2, 1])
 
 
 @time trial = randomize(nsbj, nsim, w, 314159, minqd, η = 1.);
+@time trial = randomize(nsbj, nsim, w, 314159, maxent, η = 0.25);
+@time trial = randomize_dl(nsbj, nsim, w, 314159, 2);
